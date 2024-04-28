28e7aed273d063742a12a48537724cae20e5a806
RC_Student_lab <RC_Student_lab@labAJ4H0X.fiii1o11wndepjatphkafrsvoa.fx.internal.cloudapp.net>
2024-04-28 13:22:30 +0000

    Second commit of my ice task 3 project

--- src/rc/ICETASK3LoginAndRegistrationSystem.java
+++ src/rc/ICETASK3LoginAndRegistrationSystem.java
@@ -3,14 +3,36 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package rc;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Nomcebo Zulu ST10452658
  */
 public class ICETASK3LoginAndRegistrationSystem {
+    //Create an object for the Login class
+    static Login objLogin = new Login();
 
     public static void main(String[] args) {
+        //Prompting the user for user input
+        
+        objLogin.setFirstName(JOptionPane.showInputDialog("Please enter your first name:"));
+        objLogin.setLastName(JOptionPane.showInputDialog("Please enter your last name:"));
+        objLogin.setUsername(JOptionPane.showInputDialog("Enter your username:"));
+        objLogin.setPassword(JOptionPane.showInputDialog("Enter your password:"));
+        
+        //Display message saying that username and password are successfully captured
+        objLogin.getUsername();
+        objLogin.getPassword();
+        JOptionPane.showMessageDialog(null,"Username successfully captured!");
+        JOptionPane.showMessageDialog(null, "Password successfully captured!");
+        
+        //Display welcome message
+        String firstName = objLogin.getFirstName();
+        String lastName = objLogin.getLastName();
+        JOptionPane.showMessageDialog(null, "Welcome " + firstName + " " + lastName + ", it is great to see you again!");
+        
+        
         
     }
     
--- src/rc/Login.java
+++ src/rc/Login.java
@@ -9,5 +9,40 @@ package rc;
  * @author Nomcebo Zulu ST10452658
  */
 public class Login {
+    //Declare instance variables for the user's first and last name, their username and password
+    private String firstName = "";
+    private String lastName = "";
+    private String username = "";
+    private String password = "";
+    
+    //Create the getters - the accessor methods for the instance variables to return the attributes value
+    public String getFirstName() {
+        return this.firstName;
+    }
+    public String getLastName() {
+        return this.lastName;
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    
+    //Create the setters - the mutator methods for the instance variables to be set
+    public void setFirstName (String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
+    public void setUsername (String username) {
+        this.username = username;
+    }
+    public void setPassword (String password) {
+        this.password = password;
+    }
+    
+    
     
 }
